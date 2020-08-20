c356f01c88e416872ab5df0380120f7dfef8ad9d
Man Man <chethiminhman121998@gmail.com>
2020-08-20 10:55:43 +0700

    Updates

---
+++ precache-manifest.caa0bfc510f640220d59331e32c08a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c335385de1dcdd8b1657a374989b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff0a446f80001aae44c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a9b915a063a1b5bdfe90",
+    "url": "/static/js/2.3a183c11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a183c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff0a446f80001aae44c",
+    "url": "/static/js/main.46fcaea0.chunk.js"
+  },
+  {
+    "revision": "5eaf0fa6cca8087ee00a",
+    "url": "/static/js/runtime-main.66dc504b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
